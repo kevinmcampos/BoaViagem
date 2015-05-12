711470695bf10a4216db1ed05d59a9cfaa81c299
Kevin Campos <kevinmcampos@gmail.com>
2015-05-12 17:42:46 -0300

    Adicionado tela de login

---
+++ app/src/main/java/br/com/cafecomcodigo/boaviagem/DashboardActivity.java
@@ -0,0 +1,6 @@
+package br.com.cafecomcodigo.boaviagem;
+
+import android.app.Activity;
+
+public class DashboardActivity extends Activity {
+}
---
+++ app/src/main/java/br/com/cafecomcodigo/boaviagem/LoginActivity.java
@@ -0,0 +1,41 @@
+package br.com.cafecomcodigo.boaviagem;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends ActionBarActivity {
+
+    private EditText usuario;
+    private EditText senha;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        usuario = (EditText) findViewById(R.id.usuario);
+        senha = (EditText) findViewById(R.id.senha);
+    }
+
+    public void entrarOnClick(View view) {
+
+        String usuarioInformado = usuario.getText().toString();
+        String senhaInformada = senha.getText().toString();
+        if("leitor".equals(usuarioInformado) &&
+                "123".equals(senhaInformada)) {
+
+            startActivity(new Intent(this, DashboardActivity.class));
+
+        } else{
+
+            String mensagemErro = getString(R.string.erro_autenticacao);
+            Toast toast = Toast.makeText(this, mensagemErro,
+                    Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+}
